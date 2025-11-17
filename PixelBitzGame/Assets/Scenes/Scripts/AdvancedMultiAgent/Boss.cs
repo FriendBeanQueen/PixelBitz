9c29efc0d2fefec0da228ce23e641f9e9d58100a
Allison Shelkop <allisonshelkop@gmail.com>
2025-11-17 18:49:38 -0500

    Organizing Scripts

--- PixelBitzGame/Assets/Scenes/Scripts/AdvancedMultiAgent/Boss.cs
+++ PixelBitzGame/Assets/Scenes/Scripts/AdvancedMultiAgent/Boss.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Merchant : MonoBehaviour
+public class Boss : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
